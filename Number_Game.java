6b198cc5428fe0630643fbb0d4fcc6c5eecbed01
malamba <lavhengwamalamba@gmail.com>
2024-03-25 22:26:22 +0200

    [ADDED] Prompting and multiple guesses functionality

--- Number_Game.java
+++ Number_Game.java
@@ -1,13 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
 public class Number_Game {
-    public void check_guess(int guess, int secret_num) {
-        if (guess < secret_num) {
-            System.out.println("Your Guess was too low!");
+    static Random random = new Random();
+
+    public static void check_guess(int secret_num) {
+        boolean correct = false;
+        while (!correct) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your Guess");
+            int guess = scanner.nextInt();
+            if (guess < secret_num) {
+                System.out.println("Your Guess was too low!");
+            }
+            else if (guess > secret_num) {
+                System.out.println("Your Guess was too High!");            
+            }
+            else{
+                System.out.println("Your Guess was correct");
+                correct = true;
+            }
         }
-        else if (guess < secret_num) {
-            System.out.println("Your Guess was too High!");            
-        }
-        else{
-            System.out.println("Your Guess was correct");
-        }
+    }
+    public static void main(String[] args) {
+        int secret_num = random.nextInt(100);
+        System.out.println(secret_num);
+        check_guess(secret_num);
     }
 }
