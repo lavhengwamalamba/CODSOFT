b29d6ce42662c27731f729345371d1ca723f4a7d
malamba <lavhengwamalamba@gmail.com>
2024-03-25 23:19:30 +0200

    [ADDED] Limited attemps and multiple rounds

--- Number_Game.java
+++ Number_Game.java
@@ -1,29 +1,54 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class Number_Game {
     static Random random = new Random();
+    static boolean correct = false;
+    static Scanner scanner = new Scanner(System.in);
+    static int tries;
 
-    public static void check_guess(int secret_num) {
-        boolean correct = false;
-        while (!correct) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter your Guess");
-            int guess = scanner.nextInt();
-            if (guess < secret_num) {
-                System.out.println("Your Guess was too low!");
-            }
-            else if (guess > secret_num) {
-                System.out.println("Your Guess was too High!");            
-            }
-            else{
-                System.out.println("Your Guess was correct");
-                correct = true;
-            }
+    public static void tries_check() {
+        if (tries == 1) {
+            System.out.println("You have " + tries + " more try!");
+        } else {
+            System.out.println("You have " + tries + " more tries!");
         }
     }
+
+    public static void check_guess(int secret_num) {
+        System.out.println("Enter your Guess");
+        int guess = scanner.nextInt();
+        scanner.nextLine();
+        if (guess < secret_num) {
+            System.out.println("Your Guess was too low!");
+            tries -= 1;
+        } else if (guess > secret_num) {
+            System.out.println("Your Guess was too High!");
+            tries -= 1;
+        } else {
+            System.out.println("Your Guess was correct");
+            correct = true;
+        }
+    }
+
     public static void main(String[] args) {
-        int secret_num = random.nextInt(100);
-        System.out.println(secret_num);
-        check_guess(secret_num);
+        String round = "y";
+
+        while (round == "y") {
+            tries = 3;
+            int secret_num = random.nextInt(100);
+            System.out.println(secret_num);
+            while (!correct) {
+                check_guess(secret_num);
+                tries_check();
+                if (tries == 0) {
+                    break;
+                }
+            }
+            System.out.println("Do you want to play another round?[y/n]");
+            round = scanner.nextLine();
+        }
+        System.out.println("Thank you for Playing!");
+        scanner.close();
     }
 }
