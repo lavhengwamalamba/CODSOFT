5799d347083738440d8e9fcb055d6631a9621977
malamba <lavhengwamalamba@gmail.com>
2024-03-25 21:57:38 +0200

    [ADDED] Check_guess method

--- Number_Game.java
+++ Number_Game.java
@@ -1,3 +1,13 @@
-public class Number_Game{
-    
+public class Number_Game {
+    public void check_guess(int guess, int secret_num) {
+        if (guess < secret_num) {
+            System.out.println("Your Guess was too low!");
+        }
+        else if (guess < secret_num) {
+            System.out.println("Your Guess was too High!");            
+        }
+        else{
+            System.out.println("Your Guess was correct");
+        }
+    }
 }
