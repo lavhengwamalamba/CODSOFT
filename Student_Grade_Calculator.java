e24d7e24bf3450c7315011d37fd9ce197c0bd642
malamba <lavhengwamalamba@gmail.com>
2024-04-03 22:38:07 +0200

    [ADDED] main function

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -1,5 +1,25 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Student_Grade_Calculator {
+    static ArrayList<Integer> marks = new ArrayList<Integer>();
+    static int total_marks = 0;
+    static int average = 0;
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number_of_courses = scanner.nextInt();
+        scanner.nextLine();
+        while (true) {
+            System.out.println("Enter mark (out of 100%):");
+            int mark = scanner.nextInt();
+            scanner.nextLine();
+            marks.add(mark);
+            System.out.println("Do you want to add another mark? yes/no");
+            String choice = scanner.nextLine();
+            if (choice.equalsIgnoreCase("no")) {
+                break;
+            }
+        }
+    }
 }
