6ee131b5a792c3eff13b9da60b7b1b7ab2ae5b3d
malamba <lavhengwamalamba@gmail.com>
2024-04-03 22:59:10 +0200

    [ADDED] grader() function

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -6,20 +6,39 @@ public class Student_Grade_Calculator {
     static int total_marks = 0;
     static int average = 0;
 
+    public static String grader(float mark) {
+        if (mark >= 80) {
+            return "A";
+        } else if (70 <= mark && mark < 80) {
+            return "B";
+        } else if (60 <= mark && mark < 70) {
+            return "C";
+        } else if (50 <= mark && mark < 60) {
+            return "D";
+        } else if (40 <= mark && mark < 50) {
+            return "E";
+        } else if (mark < 40) {
+            return "F";
+        }
+        return "Mark invalid";
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number_of_courses = scanner.nextInt();
-        scanner.nextLine();
         while (true) {
             System.out.println("Enter mark (out of 100%):");
             int mark = scanner.nextInt();
             scanner.nextLine();
             marks.add(mark);
+            total_marks += mark;
             System.out.println("Do you want to add another mark? yes/no");
             String choice = scanner.nextLine();
             if (choice.equalsIgnoreCase("no")) {
                 break;
             }
         }
+        float ave = total_marks / marks.size();
+        System.out.println("Total Marks: " + total_marks + "\nAverage: " + ave + "\nGrade: " + grader(ave));
+        scanner.close();
     }
 }
