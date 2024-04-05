4bc3e1066054b5733ae83ee22438bf4e7f735bca
malamba <lavhengwamalamba@gmail.com>
2024-04-05 19:58:55 +0200

    [UPDATED] grader() function

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -9,29 +9,28 @@ public class Student_Grade_Calculator {
     public static String grader(float mark) {
         if (mark >= 80) {
             return "A";
-        } else if (70 <= mark && mark < 80) {
+        } else if (mark >= 70) {
             return "B";
-        } else if (60 <= mark && mark < 70) {
+        } else if (mark >= 60) {
             return "C";
-        } else if (50 <= mark && mark < 60) {
+        } else if (mark >= 50) {
             return "D";
-        } else if (40 <= mark && mark < 50) {
+        } else if (mark >= 40) {
             return "E";
-        } else if (mark < 40) {
+        } else {
             return "F";
         }
-        return "Mark invalid";
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many subjects do you have: ");
         int count = scanner.nextInt();
-        scanner.nextLine();        
+        scanner.nextLine();
         int i = 1;
         System.out.println("Enter marks (out of 100%):");
         while (i <= count) {
-            System.out.print("" + i + ".");
+            System.out.print("" + i + ". ");
             int mark = scanner.nextInt();
             scanner.nextLine();
             marks.add(mark);
@@ -39,8 +38,8 @@ public class Student_Grade_Calculator {
             i++;
 
         }
-        float ave = total_marks / marks.size();
-        System.out.println("Total Marks: " + total_marks + "\nAverage: " + ave + "\nGrade: " + grader(ave));
+        float average = total_marks / marks.size();
+        System.out.println("Total Marks: " + total_marks + "\nAverage: " + average + "\nGrade: " + grader(average));
         scanner.close();
     }
 }
